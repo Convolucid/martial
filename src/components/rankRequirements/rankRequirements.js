5d8371ce1b72cfe2e8d8a44a3d29716ee0cc507e
Convolucid <convolucid@gmail.com>
2022-07-27 13:17:35 -0500

    Added more cards and text options

--- src/components/rankRequirements/rankRequirements.js
+++ src/components/rankRequirements/rankRequirements.js
@@ -1,5 +1,9 @@
 import './rankRequirements.css'
-import whiteTechImage from './cardImages/white-tech.png'
+import advancementImage from './cardImages/advancement.png'
+import whiteSashImage from './cardImages/white-sash.png'
+import whiteTechImage from './cardImages/white-tech.jpg'
+import whiteMindsetImage1 from './cardImages/white-mindset1.jpg'
+import whiteMindsetImage2 from './cardImages/white-mindset2.png'
 
 export default function rankRequirements(containerElement)
 {
@@ -9,7 +13,9 @@ export default function rankRequirements(containerElement)
     containerElement.appendChild(rankRequirements)
 
 
-    function createCard(cardImage, cardText)
+// Create grid container element for large card display on left
+
+    function createCard(cardImage, cardTitle, cardSubtitle)
     {
         const cardContainer = document.createElement('div')
         cardContainer.classList.add('card-container')
@@ -19,19 +25,31 @@ export default function rankRequirements(containerElement)
         card.classList.add('card-image', 'card-curriculum-view')
         cardContainer.appendChild(card)
 
-        const text = document.createElement('p')
-        text.innerText = cardText
-        text.classList.add('card-text')
-        cardContainer.appendChild(text)
+        const title = document.createElement('h3')
+        title.innerText = cardTitle
+        title.classList.add('card-title')
+        cardContainer.appendChild(title)
+
+        const subtitle = document.createElement('p')
+        subtitle.innerText = cardSubtitle
+        subtitle.classList.add('card-title', 'card-subtitle')
+        cardContainer.appendChild(subtitle)
 
 
         rankRequirements.appendChild(cardContainer)
         return cardContainer;
     }
     
-    const whiteTech8ChainPunch1 = createCard(whiteTechImage, 'Eight Chain Punch, 1st Route');
-    const whiteTechIronPhoenixCombo1 = createCard(whiteTechImage, 'Iron Phoenix Combination, 1st Route');
+    const whiteSash = createCard(whiteSashImage, 'White Sash', 'Initiate Chamber')
+    const whiteTech8ChainPunch1 = createCard(whiteTechImage, 'Eight Chain Punch', '1st Route');
+    const whiteTechIronPhoenixCombo1 = createCard(whiteTechImage, 'Iron Phoenix Combination', '1st Route');
+    const whiteMindset1 = createCard(whiteMindsetImage1, 'Tiu', 'the Warding Principle');
+    const whiteMindset2 = createCard(whiteMindsetImage2, 'Cheui Saam Sau', 'the Three Hands of Mantis');
+    const advancement = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
 
+
+
+    // Function to change 'active' card, resizing it and moving it to left, while also changing background and content
 
 
     // // Create array for class list
