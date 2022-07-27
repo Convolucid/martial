d53ad862adf92a745ad4cf92f96afb1740577949
Convolucid <convolucid@gmail.com>
2022-07-27 18:20:49 -0500

    Adjusted styles to grid organization

--- src/components/rankRequirements/rankRequirements.js
+++ src/components/rankRequirements/rankRequirements.js
@@ -1,9 +1,9 @@
 import './rankRequirements.css'
 import advancementImage from './cardImages/advancement.png'
 import whiteSashImage from './cardImages/white-sash.png'
-import whiteTechImage from './cardImages/white-tech.jpg'
-import whiteMindsetImage1 from './cardImages/white-mindset1.jpg'
-import whiteMindsetImage2 from './cardImages/white-mindset2.png'
+import whiteTechImage from './cardImages/white-tech.png'
+import whiteMindImage1 from './cardImages/white-mindset-tiu.png'
+import whiteMindImage2 from './cardImages/white-mindset-css.png'
 
 export default function rankRequirements(containerElement)
 {
@@ -13,7 +13,14 @@ export default function rankRequirements(containerElement)
     containerElement.appendChild(rankRequirements)
 
 
-// Create grid container element for large card display on left
+    // Create grid container element for large card display on left
+    const cardDisplayPanel = document.createElement('div')
+    cardDisplayPanel.classList.add('rank-requirements-card-display')
+    rankRequirements.appendChild(cardDisplayPanel)
+
+    const curriculumDisplayPanel = document.createElement('div')
+    curriculumDisplayPanel.classList.add('rank-requirements-curriculum-display')
+    rankRequirements.appendChild(curriculumDisplayPanel)
 
     function createCard(cardImage, cardTitle, cardSubtitle)
     {
@@ -22,7 +29,7 @@ export default function rankRequirements(containerElement)
 
         const card = new Image()
         card.src = cardImage
-        card.classList.add('card-image', 'card-curriculum-view')
+        card.classList.add('card-image')
         cardContainer.appendChild(card)
 
         const title = document.createElement('h3')
@@ -35,20 +42,44 @@ export default function rankRequirements(containerElement)
         subtitle.classList.add('card-title', 'card-subtitle')
         cardContainer.appendChild(subtitle)
 
-
-        rankRequirements.appendChild(cardContainer)
         return cardContainer;
     }
     
     const whiteSash = createCard(whiteSashImage, 'White Sash', 'Initiate Chamber')
-    const whiteTech8ChainPunch1 = createCard(whiteTechImage, 'Eight Chain Punch', '1st Route');
+    const whiteMindTiu = createCard(whiteMindImage1, 'Tiu', 'the Warding Principle');
+    const whiteMindCheuiSaamSau = createCard(whiteMindImage2, 'Cheui Saam Sau', 'the Three Hands of Mantis');
     const whiteTechIronPhoenixCombo1 = createCard(whiteTechImage, 'Iron Phoenix Combination', '1st Route');
-    const whiteMindset1 = createCard(whiteMindsetImage1, 'Tiu', 'the Warding Principle');
-    const whiteMindset2 = createCard(whiteMindsetImage2, 'Cheui Saam Sau', 'the Three Hands of Mantis');
-    const advancement = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteTech8ChainPunch2 = createCard(whiteTechImage, 'Eight Chain Punch', '2nd Route');
+    const whiteAdvancement = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement2 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement3 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement4 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement5 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement6 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+    const whiteAdvancement7 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
 
+    const rankRequirementsWhite = [
+        whiteSash, whiteMindTiu, whiteMindCheuiSaamSau, whiteTechIronPhoenixCombo1, whiteTech8ChainPunch2,  whiteAdvancement
+        // whiteAdvancement3, whiteAdvancement4, whiteAdvancement5, whiteAdvancement2
+    ]
 
+    function displayFocalCard(cardName)
+    {
+        for(let i=0; i < rankRequirementsWhite.length; i++)
+        {
+            if(rankRequirementsWhite[i] == cardName)
+            {
+                cardDisplayPanel.appendChild(rankRequirementsWhite[i])
+            }
+            else
+            {
+                curriculumDisplayPanel.appendChild(rankRequirementsWhite[i])
+                rankRequirementsWhite[i].classList.add('card-curriculum-display')
+            }
+        }
+    }
 
+    displayFocalCard(whiteSash);
     // Function to change 'active' card, resizing it and moving it to left, while also changing background and content
 
 
@@ -56,10 +87,6 @@ export default function rankRequirements(containerElement)
     const rankRequirementsStyles = [
         'rank-requirements-whiteSash', 'rank-requirements-yellowSash', 'rank-requirements-orangeSash', 'rank-requirements-purpleSash', 'rank-requirements-blueSash', 'rank-requirements-greenSash', 'rank-requirements-brownSash', 'rank-requirements-goldSash', 'rank-requirements-redSash', 'rank-requirements-blackSash'
     ]
-
-    // const rankTitleText = [
-    //     'White Sash', 'Yellow Sash', 'Orange Sash', 'Purple Sash', 'Blue Sash', 'Green Sash', 'Brown Sash', 'Gold Sash', 'Red Sash', 'Black Sash'
-    // ]
 
 
     // Style-switching functionality
