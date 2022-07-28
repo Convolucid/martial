4836bd2db111b58d9db61c8530b0917f7101b942
Convolucid <convolucid@gmail.com>
2022-07-27 22:14:03 -0500

    Added rankRequirementPanel system

--- src/components/rankRequirements/rankRequirements.js
+++ src/components/rankRequirements/rankRequirements.js
@@ -4,23 +4,31 @@ import whiteSashImage from './cardImages/white-sash.png'
 import whiteTechImage from './cardImages/white-tech.png'
 import whiteMindImage1 from './cardImages/white-mindset-tiu.png'
 import whiteMindImage2 from './cardImages/white-mindset-css.png'
+import yellowSashImage from './cardImages/yellow-sash.png'
+import yellowTechImage from './cardImages/yellow-tech.png'
+import yellowMindImage1 from './cardImages/yellow-mindset-jimnim.png'
+import yellowMindImage2 from './cardImages/yellow-mindset-au.png'
 
 export default function rankRequirements(containerElement)
 {
-    // Create HTML element and insert text
-    const rankRequirements = document.createElement('section')
-    rankRequirements.classList.add('rank-requirements', 'rank-requirements-whiteSash')
-    containerElement.appendChild(rankRequirements)
+    // Create HTML element section for rank requirements
+    const rankRequirementsSection = document.createElement('section')
+    rankRequirementsSection.classList.add('rank-requirements-section')
+    containerElement.appendChild(rankRequirementsSection)
+
+
+
+
 
 
     // Create grid container element for large card display on left
-    const cardDisplayPanel = document.createElement('div')
-    cardDisplayPanel.classList.add('rank-requirements-card-display')
-    rankRequirements.appendChild(cardDisplayPanel)
+    // const cardDisplayPanel = document.createElement('div')
+    // cardDisplayPanel.classList.add('rank-requirements-card-display')
+    // rankRequirements.appendChild(cardDisplayPanel)
 
-    const curriculumDisplayPanel = document.createElement('div')
-    curriculumDisplayPanel.classList.add('rank-requirements-curriculum-display')
-    rankRequirements.appendChild(curriculumDisplayPanel)
+    // const curriculumDisplayPanel = document.createElement('div')
+    // curriculumDisplayPanel.classList.add('rank-requirements-curriculum-display')
+    // rankRequirements.appendChild(curriculumDisplayPanel)
 
     function createCard(cardImage, cardTitle, cardSubtitle)
     {
@@ -51,56 +59,97 @@ export default function rankRequirements(containerElement)
     const whiteTechIronPhoenixCombo1 = createCard(whiteTechImage, 'Iron Phoenix Combination', '1st Route');
     const whiteTech8ChainPunch2 = createCard(whiteTechImage, 'Eight Chain Punch', '2nd Route');
     const whiteAdvancement = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement2 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement3 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement4 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement5 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement6 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
-    const whiteAdvancement7 = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
 
-    const rankRequirementsWhite = [
+    const whiteRankRequirements = [
         whiteSash, whiteMindTiu, whiteMindCheuiSaamSau, whiteTechIronPhoenixCombo1, whiteTech8ChainPunch2,  whiteAdvancement
-        // whiteAdvancement3, whiteAdvancement4, whiteAdvancement5, whiteAdvancement2
     ]
 
-    function displayFocalCard(cardName)
+    const yellowSash = createCard(yellowSashImage, 'Yellow Sash', 'Initiate Chamber')
+    const yellowMindJimNim = createCard(yellowMindImage1, 'Jim, Nim', 'Contact and Cling');
+    const yellowMindAu = createCard(yellowMindImage2, 'Au, Diu, Got', 'the Mantis Hook');
+    const yellowTechIronPhoenixCombo2 = createCard(yellowTechImage, 'Iron Phoenix Combination', '2nd Route');
+    const yellowTechInterceptingHammerFist = createCard(yellowTechImage, 'Intercepting Hammer Fist');
+    const yellowAdvancement = createCard(advancementImage, 'Advancement', 'Initiate Tier or higher')
+
+    const yellowRankRequirements = [
+        yellowSash, yellowMindJimNim, yellowMindAu, yellowTechIronPhoenixCombo2, yellowTechInterceptingHammerFist,  yellowAdvancement
+    ]
+
+
+    // Create panels from rank requirement arrays
+    function createRankRequirementsPanel(array)
     {
-        for(let i=0; i < rankRequirementsWhite.length; i++)
+        const rankRequirements = document.createElement('article')
+        rankRequirements.classList.add('rank-requirements')
+        rankRequirementsSection.appendChild(rankRequirements)
+
+        const cardDisplayPanel = document.createElement('div')
+        cardDisplayPanel.classList.add('rank-requirements-card-display')
+        rankRequirements.appendChild(cardDisplayPanel)
+
+        const curriculumDisplayPanel = document.createElement('div')
+        curriculumDisplayPanel.classList.add('rank-requirements-curriculum-display')
+        rankRequirements.appendChild(curriculumDisplayPanel)
+
+        for(let i=0; i < array.length; i++)
         {
-            if(rankRequirementsWhite[i] == cardName)
+            if(i == 0)
             {
-                cardDisplayPanel.appendChild(rankRequirementsWhite[i])
+                cardDisplayPanel.appendChild(array[0])
             }
             else
             {
-                curriculumDisplayPanel.appendChild(rankRequirementsWhite[i])
-                rankRequirementsWhite[i].classList.add('card-curriculum-display')
+                curriculumDisplayPanel.appendChild(array[i])
+            }
+        }
+
+        return rankRequirements;
+    }
+
+    const whiteRankRequirementsPanel = createRankRequirementsPanel(whiteRankRequirements)
+    const yellowRankRequirementsPanel = createRankRequirementsPanel(yellowRankRequirements)
+
+    const rankRequirementsPanelArray = [
+        whiteRankRequirementsPanel, yellowRankRequirementsPanel
+    ]
+
+
+    // Function to choose panel to display
+    function displayCurriculum(panel)
+    {
+        for(let i=0; i < rankRequirementsPanelArray.length; i++)
+        {
+            if(rankRequirementsPanelArray[i] == panel)
+            {
+                rankRequirementsPanelArray[i].classList.remove('rank-requirements-hidden')
+            }
+            else
+            {
+                rankRequirementsPanelArray[i].classList.add('rank-requirements-hidden')
             }
         }
     }
 
-    displayFocalCard(whiteSash);
-    // Function to change 'active' card, resizing it and moving it to left, while also changing background and content
-
-
-    // Create array for class list
+    // Create array for style class list
     const rankRequirementsStyles = [
         'rank-requirements-whiteSash', 'rank-requirements-yellowSash', 'rank-requirements-orangeSash', 'rank-requirements-purpleSash', 'rank-requirements-blueSash', 'rank-requirements-greenSash', 'rank-requirements-brownSash', 'rank-requirements-goldSash', 'rank-requirements-redSash', 'rank-requirements-blackSash'
     ]
 
 
     // Style-switching functionality
-    rankRequirements.rankChange = function(rank)
+    rankRequirementsSection.rankChange = function(rank)
     {
         for(let i=0; i < rankRequirementsStyles.length; i++)
         {
-            rankRequirements.classList.remove(rankRequirementsStyles[i]);
+            rankRequirementsSection.classList.remove(rankRequirementsStyles[i]);
         }
 
-        rankRequirements.classList.add(rankRequirementsStyles[rank])
+        displayCurriculum(rankRequirementsPanelArray[rank])
+        rankRequirementsSection.classList.add(rankRequirementsStyles[rank])
+
     }
 
-    // rankTitle.rankChange(0);
+    rankRequirementsSection.rankChange(0);
 
-    return rankRequirements;
+    return rankRequirementsSection;
 }
