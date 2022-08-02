ccea5eac7639a57009d4a2d1cd3443efeaeb080e
Convolucid <convolucid@gmail.com>
2022-08-02 00:09:57 -0500

    Fixed black sash links

--- src/components/cardLibrary/tier4-adept/rank10Black.js
+++ src/components/cardLibrary/tier4-adept/rank10Black.js
@@ -4,151 +4,141 @@ import Challenge from '../Challenge.js'
 
 import techniqueLibrary from '../../techniqueLibrary/techniqueLibrary.js'
 import generalCurrency from '../tier0-general/generalCurrency.js'
-// import blueRankContent from './rank5Blue.js'
+// import blackRankContent from '../tier4-adept/rank10Black.js'
 
-// import greenContent from './rank6-green/green-content.md'
-// import greenChallenges from './rank6-green/green-challenges.js'
-import greenSashImage from './rank6-green/green-sash.png'
-import greenMindImage1 from './rank6-green/green-mind-lengheng.png'
-import greenMindImage2 from './rank6-green/green-mind-root.png'
-import greenMindImage3 from './rank6-green/green-mind-wancham.png'
-import greenTechImage from './rank6-green/green-tech-mantis.png'
-import greenTrainingManualImage from './rank6-green/green-trainingManual.png'
-import proficiencyBronzeChoreoImage from '../tier0-general/proficiency-bronze-choreo.png'
-import proficiencySilverInitiateImage from '../tier0-general/proficiency-silver-initiate.png'
+import forgeInvitationImage from '../tier0-general/currency/forge-invitation.png'
 
 
-export default function greenRankContent()
+export default function blackRankContent()
 {
-    const r6 = {}
-    // const r5 = blueRankContent()
+    const r10 = {}
 
     const t = techniqueLibrary()
-    // const ch = greenChallenges()
+    // const ch = redChallenges()
     const gen = generalCurrency()
 
 
     const container = document.createElement('div')
-    // container.insertAdjacentHTML('beforeend', greenContent)
+    // container.insertAdjacentHTML('beforeend', redContent)
 
     // Sash
-    r6.sash = {
-        title: 'Green Sash',
-        subtitle: 'Development Chamber',
-        image: greenSashImage,
+    r10.sash = {
+        title: 'Black Sash',
+        subtitle: 'Adept Chamber',
+        image: forgeInvitationImage,
     }
 
-    // Technical 1 - Tong Long Bou Syun
-    r6.tech1 = {
-        title: 'Tong Long Bou Syun',
-        subtitle: 'Mantis Whirlwind Step',
-        image: greenTechImage,
-        // description: container.querySelector('#r6-tech1-desc'),
-        techniques: [
-            new Technique(t.fist2), 
-            new Technique(t.fist3), 
-            new Technique(t.kick1), 
-            new Technique(t.fist4), 
-            new Technique(t.hand2), 
-            new Technique(t.hand5), 
-            new Technique(t.step1), 
-            new Technique(t.step2)
-        ],
-        // challenges: [
-        //     new Challenge(ch.a),
-        //     new Challenge(ch.a),
-        //     new Challenge(ch.a),
-        //     new Challenge(ch.a),
-        //     new Challenge(ch.a),
-        // ],
-        rewards: [
-            new PathCard(gen.advancementInvitation),
-            new PathCard(gen.iron10)
-        ]
-    }
-
-    // Mindset 1 - Leng, Heng
-    r6.mind1 = {
-        title: 'Leng, Heng',
-        subtitle: 'Lightness and Agility',
-        image: greenMindImage1,
-        // description: container.querySelector('#r6-mind1-desc'),
-        // challenges: [container.querySelector('#r6-mind1-tasks')],
-        rewards: [
-            new PathCard(r6.tech1),
-            new PathCard(gen.iron10)
-        ]
-    }
-
-    // Training Manual - Green Dragon Temple
-    r6.manual = {
-        title: 'Green Dragon Temple',
-        subtitle: 'Choose one Technical card',
-        image: greenTrainingManualImage,
-        // description: container.querySelector('#r6-tech2-desc'),
-        // techniques: [
-        //     new Technique(t.hand2), 
-        //     new Technique(t.hand3), 
-        //     new Technique(t.hand5), 
-        //     new Technique(t.fist3), 
-        //     new Technique(t.fist4), 
-        //     new Technique(t.fist5), 
-        //     new Technique(t.step3), 
-        //     new Technique(t.step4), 
-        //     new Technique(t.step5)
-        // ],
-        // challenges: '',
-        rewards: [
-            new PathCard(gen.advancementInvitation),
-            new PathCard(gen.iron10)
-        ]
-    }
-
-    // Mindset 2 - Root
-    r6.mind2 = {
-        title: 'Root',
+    // Mystery card
+    r10.mystery = {
+        title: '?',
         subtitle: '',
-        image: greenMindImage2,
-        // description: container.querySelector('#r6-mind2-desc'),
-        // challenges: [container.querySelector('#r6-mind2-tasks')],
-        rewards: [
-            new PathCard(r6.manual),
-            new PathCard(gen.iron10)
-        ]
+        image: forgeInvitationImage,
+        // description: container.querySelector('#r10-tech1-desc'),
+        // techniques: [
+        //     new Technique(t.fist2), 
+        //     new Technique(t.fist3), 
+        //     new Technique(t.kick1), 
+        //     new Technique(t.fist4), 
+        //     new Technique(t.hand2), 
+        //     new Technique(t.hand5), 
+        //     new Technique(t.step1), 
+        //     new Technique(t.step2)
+        // ],
+        // // challenges: [
+        // //     new Challenge(ch.a),
+        // //     new Challenge(ch.a),
+        // //     new Challenge(ch.a),
+        // //     new Challenge(ch.a),
+        // //     new Challenge(ch.a),
+        // // ],
+        // rewards: [
+        //     new PathCard(gen.advancementInvitation),
+        //     new PathCard(gen.iron10)
+        // ]
     }
 
-    // Mindset 3 - Wan, Cham
-    r6.mind3 = {
-        title: 'Wan, Cham',
-        subtitle: 'Stability and Sinking',
-        image: greenMindImage3,
-        // description: container.querySelector('#r6-mind2-desc'),
-        // challenges: [container.querySelector('#r6-mind2-tasks')],
-        rewards: [
-            new PathCard(r6.manual),
-            new PathCard(gen.iron10)
-        ]
-    }
+    // // Mindset 1 - Leng, Heng
+    // r10.mind1 = {
+    //     title: 'Leng, Heng',
+    //     subtitle: 'Lightness and Agility',
+    //     image: redMindImage1,
+    //     // description: container.querySelector('#r10-mind1-desc'),
+    //     // challenges: [container.querySelector('#r10-mind1-tasks')],
+    //     rewards: [
+    //         new PathCard(r10.tech1),
+    //         new PathCard(gen.iron10)
+    //     ]
+    // }
+
+    // // Training Manual - red Dragon Temple
+    // r10.manual = {
+    //     title: 'red Dragon Temple',
+    //     subtitle: 'Choose one Technical card',
+    //     image: redTrainingManualImage,
+    //     // description: container.querySelector('#r10-tech2-desc'),
+    //     // techniques: [
+    //     //     new Technique(t.hand2), 
+    //     //     new Technique(t.hand3), 
+    //     //     new Technique(t.hand5), 
+    //     //     new Technique(t.fist3), 
+    //     //     new Technique(t.fist4), 
+    //     //     new Technique(t.fist5), 
+    //     //     new Technique(t.step3), 
+    //     //     new Technique(t.step4), 
+    //     //     new Technique(t.step5)
+    //     // ],
+    //     // challenges: '',
+    //     rewards: [
+    //         new PathCard(gen.advancementInvitation),
+    //         new PathCard(gen.iron10)
+    //     ]
+    // }
+
+    // // Mindset 2 - Root
+    // r10.mind2 = {
+    //     title: 'Root',
+    //     subtitle: '',
+    //     image: redMindImage2,
+    //     // description: container.querySelector('#r10-mind2-desc'),
+    //     // challenges: [container.querySelector('#r10-mind2-tasks')],
+    //     rewards: [
+    //         new PathCard(r10.manual),
+    //         new PathCard(gen.iron10)
+    //     ]
+    // }
+
+    // // Mindset 3 - Wan, Cham
+    // r10.mind3 = {
+    //     title: 'Wan, Cham',
+    //     subtitle: 'Stability and Sinking',
+    //     image: redMindImage3,
+    //     // description: container.querySelector('#r10-mind2-desc'),
+    //     // challenges: [container.querySelector('#r10-mind2-tasks')],
+    //     rewards: [
+    //         new PathCard(r10.manual),
+    //         new PathCard(gen.iron10)
+    //     ]
+    // }
     
-    // Stance Routine Bronze Proficiency Requirement
-    r6.prof1 = {
-        title: 'Stance Routine',
-        subtitle: 'Bronze Proficiency or Higher',
-        image: proficiencyBronzeChoreoImage
-    }
+    // // Stance Routine Bronze Proficiency Requirement
+    // r10.prof1 = {
+    //     title: 'Stance Routine',
+    //     subtitle: 'Bronze Proficiency or Higher',
+    //     image: proficiencyBronzeChoreoImage
+    // }
 
-    // Stance Routine Bronze Proficiency Requirement
-    r6.prof2 = {
-        title: 'Silver Proficiency',
-        subtitle: 'Initiate Tier or Higher',
-        image: proficiencySilverInitiateImage
-    }
+    // // Stance Routine Bronze Proficiency Requirement
+    // r10.prof2 = {
+    //     title: 'Silver Proficiency',
+    //     subtitle: 'Initiate Tier or Higher',
+    //     image: proficiencySilverInitiateImage
+    // }
 
     // Advancement Invitation
-    r6.adv = gen.advancementTier2
-    // r6.adv.rewards = [
+    r10.adv = gen.advancementTier4
+    // r10.adv.rewards = [
     //         new PathCard(r7.sash)
     //     ]
 
-    return r6;
+    return r10;
 }
--- src/components/cardLibrary/tier4Collection.js
+++ src/components/cardLibrary/tier4Collection.js
@@ -1,6 +1,7 @@
 import PathCard from './PathCard.js'
 
 import generalCurrency from './tier0-general/generalCurrency.js'
+import blackRankContent from './tier4-adept/rank10Black.js'
 
 import forgeInvitationImage from './tier0-general/currency/forge-invitation.png'
 
@@ -9,18 +10,19 @@ export default function adeptTier()
     const t4 = {}
 
     const t0 = generalCurrency()
+    const r10 = blackRankContent()
 
-    t4.blackSash = new PathCard({}, forgeInvitationImage, 'Black Sash', 'Adept Chamber')
-    t4.blackMind1= new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackMind2 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackMind3 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackMind4 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackMind5 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackTech1 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackTech2 = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackTech3 = new PathCard({}, forgeInvitationImage, '?', '')
-    t4.blackTrainingManual = new PathCard({}, forgeInvitationImage, '?', '');
-    t4.blackAdvancement = new PathCard(t0.advancementTier4)
+    t4.blackSash = new PathCard(r10.sash)
+    t4.blackMind1= new PathCard(r10.mystery);
+    t4.blackMind2 = new PathCard(r10.mystery);
+    t4.blackMind3 = new PathCard(r10.mystery);
+    t4.blackMind4 = new PathCard(r10.mystery);
+    t4.blackMind5 = new PathCard(r10.mystery);
+    t4.blackTech1 = new PathCard(r10.mystery);
+    t4.blackTech2 = new PathCard(r10.mystery);
+    t4.blackTech3 = new PathCard(r10.mystery)
+    t4.blackTrainingManual = new PathCard(r10.mystery);
+    t4.blackAdvancement = new PathCard(r10.adv)
 
     t4.black = [
         t4.blackSash, t4.blackMind1, t4.blackMind2, t4.blackMind3, t4.blackMind4, t4.blackMind5, t4.blackTech1, t4.blackTech2, t4.blackTech3, t4.blackTrainingManual, t4.blackAdvancement
